f3843780311cc2ccc409944e694d61f8d1974a76
BRIVAL-M <brivalmickael@gmail.com>
2022-11-03 14:32:30 +0100

    horloge ok

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ setInterval(()=>{
     //     h = h -12
     // }
     // add 0 before single digit number
-    // h = (h < 10) ? "0" + h : h;
+    h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
